02746177474d902c1071ac7393250c87392075f7
Michael Cook <michael.cook@techdept.co.uk>
2018-01-16 17:33:23 +0000

    Rebuilding from Angular -> React (maybe Inferno but probably not)

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+// import Tome from "tome"; // <- When it's a node_module
+import Tome from "./tome";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    let tome = new Tome();
+    console.log(tome);
+    this.state = {
+      header: tome.getHeader()
+    };
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Tome Use Example</h1>
+        </header>
+        <main>
+          <p>{this.state.header}</p>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/tome/index.js
@@ -0,0 +1,11 @@
+// import React, { Component } from "react";
+// export default class Graph extends Component {
+
+export default class Tome {
+  constructor() {
+    console.log("hey");
+  }
+  getHeader() {
+    return "Testing node_module concept";
+  }
+}
