910e2559ee6a4e333d63c2b4b9d7aef586ac2092
Michael Cook <michael.cook@techdept.co.uk>
2018-02-21 14:30:50 +0000

    Fricking git and its filename capitalisation ignorance

--- src/about/About.render.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default parent => (
-  <div>
-    <h1>About</h1>
-
-    <p className="path">/src/About/About.render.js</p>
-  </div>
-);
--- src/about/about.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-import AboutRender from "./About.render";
-
-export default class extends Component {
-  render() {
-    return AboutRender(this);
-  }
-}
