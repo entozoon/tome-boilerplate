5d4cb35f532b17f5a00a06c3bfb5995610629e05
Myke Cook <michael.cook@techdept.co.uk>
2018-02-23 09:44:29 +0000

    Updating for snippets

--- src/ArticleComponents/Listing.render.js
+++ src/ArticleComponents/Listing.render.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default (parent, tome, article) => (
   <div className="article__listing">
     <Link to={article.url}>{article.title}</Link>
-    <div>{tome.snippet(article.content, 20)}</div>
+    <div>{tome.snippet(article.content, 50)}</div>
 
     <p className="path">/src/ArticleComponents/Listing.render.js</p>
   </div>
